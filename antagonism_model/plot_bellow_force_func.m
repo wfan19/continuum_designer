59d18f156583bcd3b9c033c09ec091a36a192642
Bill Fan <fanbenlong@gmail.com>
2023-06-30 11:12:42 -0400

    Separated bellows force function into standalone file

--- antagonism_model/plot_bellow_force_func.m
+++ antagonism_model/plot_bellow_force_func.m
@@ -45,45 +45,12 @@ end
 function F = bellow_force_func(S, P)
     F = zeros(size(P));
 
-    % Passive compression plane
-    % p0 = [0; 0; 0];
-    % p1 = [-0.142; 0; 1.1];    % Passive compression
-    % p2 = [0; 20; 10];         % Force when inflated and length unchanged
-    % plane_normal = null([p0'; p1'; p2']);
-    n_compression = [0.9897, -0.0639, 0.1278];
-
-    % Actuation plane
-    % p0 = [0; 0; 0];
-    % p1 = [0; 20; 10];         % Force when inflated and length unchanged
-    % p2 = [0.50; 20; 0];       % Free extension line
-    % plane_normal = null([p0'; p1'; p2']);
-    n_actuation = [0.9984   -0.0250    0.0499];
-
-    % Passive extension plane
-    % p0 = [0; 0; 0];
-    % p2 = [0.50; 20; 0];       % Free extension line
-    % p1 = [0.142; 0; -3];      % Passive extension
-    % plane_normal = null([p0'; p1'; p2']);
-    n_extension = [0.9986   -0.0250   0.0473];
-
     for i = 1 : size(P, 1)
         for j = 1 : size(P, 2)
             s = S(i, j);
             p = P(i, j);
 
-            if s > 0
-                if p/s < 20/0.5
-                    % Extension - passive and active
-                    n = n_extension;
-                else
-                    % Actuation
-                    n = n_actuation;
-                end
-            else
-                % Compression - passive and active
-                n = n_compression;
-            end
-            F(i, j) = -dot(n(1:2), [s, p]) / n(3);
+            F(i, j) = bellowForce(s, p);
         end
     end
 end
