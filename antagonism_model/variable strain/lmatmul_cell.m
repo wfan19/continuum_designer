b3b473864c3fefd6bb74b6ddcd912385c6fb08f4
Bill Fan <fanbenlong@gmail.com>
2023-07-05 13:42:43 -0400

    Cleaned up intermediary scripts that are now junk

--- antagonism_model/variable strain/lmatmul_cell.m
+++
@@ -1,4 +0,0 @@
-function cell_mats_out = lmatmul_cell(A, cell_mats_in)
-    cell_mats_out = cellfun(@(cell_mats) A * cell_mats, cell_mats_in, 'uniformoutput', false);
-end
-
--- antagonism_model/variable strain/rmatmul_cell.m
+++
@@ -1,4 +0,0 @@
-function cell_mats_out = rmatmul_cell(cell_mats_in, A)
-    cell_mats_out = cellfun(@(cell_mats) cell_mats * A, cell_mats_in, 'uniformoutput', false);
-end
-
