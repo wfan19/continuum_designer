eda7c3d496e65dfe18070fd500129a1316fa97dd
Bill Fan <fanbenlong@gmail.com>
2023-07-05 11:41:51 -0400

    Plot external force vector

--- antagonism_model/variable strain/variable_strain_equilibrium.m
+++ antagonism_model/variable strain/variable_strain_equilibrium.m
@@ -21,39 +21,46 @@ base_segment.n_spacers = 2;
 % Create the discretized variable strain arm
 arm = variable_strain_segment(N_segments, base_segment);
 
-% Test setting the twist vectors along the rod
 %% Solve the statics of the 3-muscle arm
+% Define the loading functions
+% TODO: These should be a part of the variable_strain_segment, or part of
+% the arm?
 f_force_outer = @(strain, pressure) actuatorForce_key(strain, pressure);
 f_force_inner = @(strain, pressure) actuatorForce_key(strain, pressure);
 force_funcs = {fitresult, @actuatorForce_key, @actuatorForce_key, fitresult};
 
-pressures = [0, 40, 0, 10];
-A = [
-    1, 1, 1, 1;
-    0, 0, 0, 0;
-    rho, rho*2/3, -rho*2/3, -rho;
-];
-
+% Define the scenario
+pressures = [0, 40, 0, 20];
 Q = [0; -1; 0];
 
+% Initialize the problem
 g_circ_right_initial = zeros(3, N_segments);
 g_circ_right_initial(1, :) = l_0;
-f_equilibrium = @(v_g_circ_right) check_equilibrium_with_shear(v_g_circ_right, Q, arm, force_funcs, pressures, l_0, A);
+f_equilibrium = @(v_g_circ_right) check_equilibrium_with_shear(v_g_circ_right, Q, arm, force_funcs, pressures, l_0);
 options = optimoptions('fsolve',"MaxFunctionEvaluations", 1e5);
+
+tic
 equilibrium_soln = fsolve(f_equilibrium, g_circ_right_initial, options);
+toc
 
 disp("Final residuals: ")
 disp(f_equilibrium(equilibrium_soln));
 
 arm.set_base_curve(equilibrium_soln);
 
-%%% Plot the arm
+%% Plot the arm
 ax = axes(figure());
 border_length_cm = 45;
 axis equal
+hold on
 arm.plot(ax);
 
-function v_residuals = check_equilibrium_with_shear(v_g_circ_right, Q, arm_obj, force_funcs, pressures, l_0, A)
+% Create the arrow for the tip force
+xy = SE2.translation(arm.get_tip_pose());
+uv = Q(1:2) * l_0/10;
+quiver(xy(1), xy(2), uv(1), uv(2), "off", "linewidth", 3, "MaxHeadSize", 1);
+
+function v_residuals = check_equilibrium_with_shear(v_g_circ_right, Q, arm_obj, force_funcs, pressures, l_0)
     N_segments = length(arm_obj.arms);
     g_circ_right = reshape(v_g_circ_right, [3, N_segments]);
     arm_obj.set_base_curve(g_circ_right);
@@ -63,12 +70,26 @@ function v_residuals = check_equilibrium_with_shear(v_g_circ_right, Q, arm_obj, 
     internal_forces = zeros(3, N_segments);
     for i = 1 : N_segments
         segment = arm_obj.arms{i};
-        forces_i = zeros(length(segment.muscles), 1);
-        for j = 1 : length(segment.muscles)
+        N_muscles = length(segment.muscles);
+        forces_i = zeros(N_muscles, 1);
+
+        % Find the strain and thus corresponding force in each muscle
+        for j = 1 : N_muscles
             muscle_j = segment.muscles(j);
             epsilon_j = (muscle_j.l - l_0) / l_0;
             forces_i(j) = force_funcs{j}(epsilon_j, pressures(j));
         end
+
+        % Use the segment geometry to build the equilibrium matrix
+        % TODO: this should be part of an arm or segment?
+        g_o_muscles = {segment.muscles.g_o_i};
+        t_o_muscles = cell2mat(cellfun(@(mat) SE2.translation(mat), g_o_muscles, "uniformoutput", false));
+        dy_o_muscles = t_o_muscles(2, :);
+        A = zeros(3, N_muscles);
+        A(1, :) = 1;
+        A(3, :) = dy_o_muscles;
+
+        % Calculate the total internal force vector
         internal_forces(:, i) = A * forces_i;
     end
 
