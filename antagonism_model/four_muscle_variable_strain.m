afbc591fcef5bc0014ba0c9c7e5511cfa81e73c6
Bill Fan <fanbenlong@gmail.com>
2023-07-19 10:39:25 -0400

    Removed the need to run make_bellow_force_func

--- antagonism_model/four_muscle_variable_strain.m
+++ antagonism_model/four_muscle_variable_strain.m
@@ -5,25 +5,7 @@ N_segments = 10;
 l_0 = 0.5; % Default length
 
 rho = 1 * 0.0254; % Define inter-muscle geometry
-g_o_X = SE2.hat([0, rho, 0]);
-g_o_A = SE2.hat([0, rho * 1/3, 0]);
-g_o_B = SE2.hat([0, -rho * 1/3, 0]);
-g_o_Y = SE2.hat([0, -rho, 0]);
-g_o_muscles = {g_o_X; g_o_A; g_o_B; g_o_Y};
-
-g_0_o = SE2.hat([0, 0, -pi/2]);
-g_0_muscles = lmatmul_cell(g_0_o, g_o_muscles); % g_i(0) = g_o(0) * g_oi
-
-base_segment = Arm2D(g_0_o, g_o_muscles, l_0, 'plot_unstrained', false);
-base_segment.rho = rho;
-base_segment.n_spacers = 2;
-
-f_force_outer = @(strain, pressure) actuatorForce_key(strain, pressure);
-f_force_inner = @(strain, pressure) actuatorForce_key(strain, pressure);
-force_funcs = {fitresult, @actuatorForce_key, @actuatorForce_key, fitresult};
-
-% Create the discretized variable strain arm
-arm = variable_strain_segment(N_segments, base_segment, force_funcs);
+arm = make_default_arm(rho, N_segments, l_0);
 
 %% Solve the statics of the 3-muscle arm
 
