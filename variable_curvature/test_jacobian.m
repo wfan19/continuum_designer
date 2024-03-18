951b4e0d8236aaf9943b5d6df90b087598af40ff
Bill Fan <fanbenlong@gmail.com>
2024-03-17 21:07:31 -0400

    Implemented 5-seg external loading jacobian

--- variable_curvature/test_jacobian.m
+++ variable_curvature/test_jacobian.m
@@ -2,12 +2,7 @@ function tests = test_jacobian
 tests = functiontests(localfunctions);
 end
 
-function test_reaction_jacobian(testCase)
-    %%% Create a set of random linearization points
-    % Create the random base-curve curvatures to linearize around
-    N_segs = 10;
-    N_test_pts = 10;
-    rng(0);
+function test_segment_twists = create_test_twists(N_segs, N_test_pts)
     test_segment_twists = rand([3, N_segs, N_test_pts]);
     for i = 1 : N_test_pts
         % Go through each twist matrix and scale the random values to
@@ -26,9 +21,23 @@ function test_reaction_jacobian(testCase)
         c = [length_min; shear; curvature_min];
         test_segment_twists(:, :, i) = K * test_segment_twists(:, :, i) + c;
     end
-    
-    % Test arm and pressures
-    p = [10; 20];
+end
+
+function [df, dx, J_x] = compute_numeric_jacobian(func, test_x, dx_min, dx_max)
+    dx_range = dx_max - dx_min;
+    dx = dx_range * rand(size(test_x)) + dx_min;
+    test_x_perturbed = test_x + dx;
+
+    [f_x, J_x] = func(test_x);
+    [f_x_perturbed, ~] = func(test_x_perturbed);
+
+    df = f_x_perturbed - f_x;
+end
+
+function test_reaction_jacobian(testCase)   
+    %%% Declare baseline test arm design and muscle pressures
+    % TODO: test for different pressures?
+    p = [60; 50];
     struct_design = struct();
     struct_design.g_0 = Pose2.hat([0, 0, -pi/2]);
     struct_design.rhos = [-0.05, 0.05];
@@ -43,34 +52,56 @@ function test_reaction_jacobian(testCase)
         0, 0;
         struct_design.rhos;
     ];
+
+    %%% Create a set of random linearization points
+    % Create the random base-curve curvatures to linearize around
+    N_segs = 10;
+    N_test_pts = 10;
+    rng(0);
+    test_segment_twists = create_test_twists(N_segs, N_test_pts);
     
+    f_reaction = @(test_twists) calc_reaction_wrench(test_twists, p, struct_design);
     for i = 1 : N_test_pts
         %%% For each linearization point, take a random small step in x, and track
         % how that changes the output
         test_twists_i = test_segment_twists(:, :, i);
+        [d_a_numeric, d_g_circ, J_x] = compute_numeric_jacobian(f_reaction, test_twists_i, -1e-3, 1e-3);
         
-        [rxn_wrench_g_circ, J] = calc_reaction_wrench(test_twists_i, p, struct_design);
-       
-        % Generate a random small change in base curve shape
-        d_g_circ_max = 1e-3;
-        d_g_circ_min = -1e-3;
-        range = d_g_circ_max - d_g_circ_min;
-        d_g_circ = range*rand([3, N_segs]) + d_g_circ_min;
-        test_twists_i_perturbed = test_twists_i + d_g_circ; % Slightly modified base-curve
-
-        % Compute the reaction at the slightly different base-curve
-        rxn_wrench_perturbed = calc_reaction_wrench(test_twists_i_perturbed, p, struct_design);
-        
-        d_a_numeric = rxn_wrench_perturbed - rxn_wrench_g_circ;
-        fprintf("Test %d", i);
-
         %%% Compare to the output from J*dx using the analytic implementation
         % First we need to construct the analytic Jacobian
-        d_a_analytic = J * d_g_circ(:);
+        d_a_analytic = J_x * d_g_circ(:);
+        fprintf("Test %d", i);
         disp([d_a_numeric(:), d_a_analytic]);
     end
 end
 
 function test_external_jacobian(testCase)
+    %%% Define baseline test arm design and external load
+    theta_0 = -pi/2;
+    g_0 = Pose2.hat([0, 0, theta_0]);
+    Q = [0; 0; -10];
 
+    %%% Create a set of random linearization points
+    % Create the random base-curve curvatures to linearize around
+    N_segs = 5;
+    N_test_pts = 10;
+    rng(0);
+    test_segment_twists = create_test_twists(N_segs, N_test_pts);
+    
+    f_reaction = @(test_twists) deal(...
+        calc_external_wrench(test_twists, Q, g_0), ...
+        J_external_5seg(theta_0, Q, test_twists) ...
+    );
+    for i = 1 : N_test_pts
+        %%% For each linearization point, take a random small step in x, and track
+        % how that changes the output
+        test_twists_i = test_segment_twists(:, :, i);
+        [d_a_numeric, d_g_circ, J_x] = compute_numeric_jacobian(f_reaction, test_twists_i, -1e-3, 1e-3);
+        
+        %%% Compare to the output from J*dx using the analytic implementation
+        % First we need to construct the analytic Jacobian
+        d_a_analytic = J_x * d_g_circ(:);
+        fprintf("Test %d", i);
+        disp([d_a_numeric(:), d_a_analytic]);
+    end
 end
