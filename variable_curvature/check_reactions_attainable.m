cf31476e859e36982bc81c2d25b61b61181f1bbb
Bill Fan <fanbenlong@gmail.com>
2024-04-26 00:02:52 -0400

    Fast reaction-attainability function implmented

---
+++ variable_curvature/check_reactions_attainable.m
@@ -0,0 +1,58 @@
+function [v_attainable, min_dists, p_solns] = check_reactions_attainable(q_tests, segment_twists, struct_design, p_bounds, threshold)
+    % Check if a series of reactions are actually attainable by an arm
+    % Inputs:
+    %   - q_tests (3 x N_segments x N_test_points): a set of test reaction
+    %   requirements at each point along the arm
+    %   - segment_twists (3 x N_segments): twist vector at each segment
+    %   - struct_design (struct): standard arm design struct
+    %   - p_bounds (N_actuators x 1): max pressures for each actuator
+    %
+    % Outputs:
+    %   - v_attainable (N_test_points x 1): boolean vector indicating
+    %   whether each test arm reaction requirements are attainable
+    N_tests = size(q_tests, 3);
+    N_actuators = length(p_bounds);
+
+    v_attainable = false(N_tests, 1);
+    min_dists = zeros(N_tests, 1);
+    p_solns = zeros(length(p_bounds), N_tests);
+
+    N_starts = 5;
+    v_zero = zeros(N_actuators, 1);
+    start_points = diag(p_bounds) * rand([N_actuators, N_starts - 2]);
+    start_points = [start_points, v_zero, p_bounds];
+
+    for i = 1 : N_tests
+        % Define the cost function: for each pressure, find how close it
+        % gets the reactions to those required.
+        f_reaction_dist = @(pres) reaction_dists(pres, q_tests(:, :, i));
+        opts = optimoptions('fmincon', 'display', 'off', 'StepTolerance', 1e-20, 'FunctionTolerance', 1e-10); 
+
+        % Solve the optimization problem
+        p_soln_starts = zeros(N_actuators, N_starts);
+        min_dists_starts = zeros(N_starts, 1);
+        for i_start = 1 : N_starts
+            p_start = start_points(:, i_start);
+            [p_soln_starts(:, i_start), min_dist_starts(i_start), ~, output] = fmincon(f_reaction_dist, p_start, [], [], [], [], v_zero, p_bounds, [], opts); 
+        end
+
+        [min_dists(i), i_start_min] = min(min_dist_starts);
+        p_solns(:, i) = p_soln_starts(:, i_start_min);
+
+        % If the minimum distance is below a threshold, then the reaction
+        % is reachable and thus return true.
+        v_attainable(i) = min_dists(i) < threshold;
+    end
+
+    function dist = reaction_dists(pressure, q_test)
+        reactions = calc_reaction_wrench(segment_twists, pressure, struct_design);
+        residuals = q_test + reactions;
+        
+        residuals(2, :) = 1e3 * segment_twists(2, :);
+
+        %dist = sum(vecnorm(residuals, 1));
+        dist = norm(residuals(:));
+    
+    end
+end
+
---
+++ variable_curvature/check_reactions_attainable_fast.m
@@ -0,0 +1,107 @@
+function attainable = check_reactions_attainable_fast(reaction_requirement, segment_twists, struct_design, p_bounds)
+    
+    N_nodes = size(segment_twists, 2);
+    f_required = reaction_requirement(1, :);
+    m_required = reaction_requirement(3, :);
+
+    %%% Create the boundary reactions
+    % Generate pressures along the edges of pressure space
+    N_ps_per_edge = 2;
+    ps_bndry = sample_edges_of_cuboid(2, p_bounds);
+    N_ps = size(ps_bndry, 2);
+
+    % Compute the corresponding reactions these pressures induce, for the
+    % given arm design and segment twists
+    reactions_bndry = zeros(3, N_nodes, N_ps);
+    for i = 1 : N_ps
+        ps_i = ps_bndry(:, i);
+        reactions_bndry(:, :, i) = calc_reaction_wrench(segment_twists, ps_i, struct_design);
+    end
+    [traces_f_bndry, traces_m_bndry] = mat_wrenches_to_traces(reactions_bndry);
+    i_contour = boundary(traces_f_bndry(1, :)', traces_m_bndry(1, :)', 0.1);
+    rltv_f_bndry = traces_f_bndry - traces_f_bndry(1, :);
+    rltv_m_bndry = traces_m_bndry - traces_m_bndry(1, :);
+    
+    %%% Test the attainability of the reaction
+    % Test 1: are the reactions themselves bounded by the reactions from
+    % the boundary pressures
+    v_rxn_in_bndry = false(N_nodes, 1);
+    for i = 1 : N_nodes
+        v_rxn_in_bndry(i) = inpolygon(f_required(i), m_required(i), traces_f_bndry(i, i_contour), traces_m_bndry(i, i_contour));
+    end
+
+    % Test 2: are the changes in requierd reaction along the arm bounded by
+    % the changes in of boundary reactions along the arm?
+    % TODO: Better name for the concept of " change in required reaction"
+    % Maybe "relative (rltv) reaction"?
+    rltv_reaction_requirement = reaction_requirement - reaction_requirement(:, 1);
+    rltv_f = rltv_reaction_requirement(1, :);
+    rltv_m = rltv_reaction_requirement(3, :);
+
+    % Fast check: are all reactions within the convex hull of the boundary
+    % pressures? If all no then return no. If yes, then proceed to the
+    % check for each s
+    i_contour_all = boundary(rltv_f_bndry(:), rltv_m_bndry(:), 0);
+    contour_f_bndry = rltv_f_bndry(i_contour_all);
+    contour_m_bndry = rltv_m_bndry(i_contour_all);
+
+    v_rltv_rxn_in_bndry = inpolygon(rltv_f,rltv_m, contour_f_bndry, contour_m_bndry);
+    attainable = all(v_rltv_rxn_in_bndry);
+
+    if (attainable)
+        % If the required relative reactions are fully within the relative
+        % reactions of pressures along the 
+        v_rltv_rxn_in_bndry = false(N_nodes, 1);
+        v_rltv_rxn_in_bndry(1) = true;
+        for j = 2 : N_nodes
+            i_contour_j = boundary(rltv_f_bndry(j, :)', rltv_m_bndry(j, :)', 0.1);
+            v_rltv_rxn_in_bndry(j) = inpolygon(rltv_f(j), rltv_m(j), rltv_f_bndry(j, i_contour_j), rltv_m_bndry(j, i_contour_j));
+        end
+    
+        attainable = all(v_rxn_in_bndry) && all(v_rltv_rxn_in_bndry);
+    end
+
+    %% Functions
+    % TODO: These should be general function files for code reuse.
+    function points = sample_edges_of_cuboid(N_points_on_edge, scales)
+        % Returns points on the edges and vertices of a N-dimensional cube
+        N_dims = length(scales);
+        N_edges = N_dims * 2^(N_dims - 1);
+        N_points = N_points_on_edge * N_edges;
+    
+        bitstrings = (dec2bin(1:2^(N_dims-1)) - '0')';
+        %points = rand([N_dims, N_points]);
+        points = [];
+        t = linspace(0, 1, N_points_on_edge+1);
+        t = t(2:end-1);
+        for i_dim = 1 : N_dims
+            % Create points for all vertices that has this dimension going 0-1
+            for i_edge = 1 : length(bitstrings)
+                bitstring_i = bitstrings(:, i_edge);
+                points_edge_i = repmat(bitstring_i, 1, length(t));
+                points_edge_i(i_dim, :) = t;
+                points = [points, points_edge_i];
+            end
+        end
+    
+        vertices = (dec2bin(0:(2^N_dims - 1)) - '0')';
+        points = [points, vertices];
+    
+        points = diag(scales) * points;
+    end
+
+    function [traces_f, traces_m] = mat_wrenches_to_traces(mat_wrenches)
+        N_ps = size(mat_wrenches, 3);
+        N_nodes = size(mat_wrenches, 2);
+    
+        traces_f = zeros(N_nodes, N_ps);
+        traces_m= zeros(N_nodes, N_ps);
+        
+        for i_node = 1 : N_nodes
+            traces_f(i_node, :) = squeeze(mat_wrenches(1, i_node, :));
+            traces_m(i_node, :) = squeeze(mat_wrenches(3, i_node, :));
+        end
+    end
+
+end
+
