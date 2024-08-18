baf429f7f879f362f54253c6ea5169dea42af744
Bill Fan <fanbenlong@gmail.com>
2024-08-18 13:15:17 -0400

    Rewrote the sample_edges_of_cuboid function which I had accidentally deleted w/o adding to git

---
+++ variable_curvature/include/attainability/sample_edges_of_cuboid.m
@@ -0,0 +1,21 @@
+function points_out = sample_edges_of_cuboid(N_points_per_edge, scales)
+    N_dims = length(scales);
+
+    s_along_edge = linspace(0, 1, N_points_per_edge);
+
+    vertices = ff2n(N_dims)';
+    
+    points = [];
+    for i_vert = 1 : size(vertices, 2)
+        % Add points on each of the edges connected to this vertex
+        vert_i = vertices(:, i_vert);
+        for j_dim = 1 : N_dims
+            vert_i_repeat = repmat(vert_i, [1, N_points_per_edge]);
+            vert_i_repeat(j_dim, :) = s_along_edge;
+
+            points = [points, vert_i_repeat];
+        end
+    end
+    
+    points_out = diag(scales) * points;
+end
