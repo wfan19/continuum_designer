b3bf5ea70aa98abd5b58470c4dccf056e56ab345
Bill Fan <fanbenlong@gmail.com>
2024-09-03 08:25:01 -0400

    First draft of the attainability enclosure plot

--- variable_curvature/include/attainability/check_loads_attainable.m
+++ variable_curvature/include/attainability/check_loads_attainable.m
@@ -6,9 +6,15 @@ function in_bndry = check_loads_attainable(bndry_af, bndry_am, test_f, test_m)
     in_bndry = false(size(test_f));
 
     for i = 1 : N_nodes
+        % Obtain the half-space representation of the attainable wrench
+        % hull at each point s along the arm
         [A_i, b_i] = vert2lcon([bndry_af(i, :)', bndry_am(i, :)']);
 
+        % Format the test force and moments into a set of test points in R^2
         test_pts_i = [test_f(i, :); test_m(i, :)];
+
+        % Determine if they lie in the convex hull by using the half-space
+        % representation.
         in_bndry(i, :) = all(A_i * test_pts_i < b_i, 1);
     end
 
